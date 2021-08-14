f8102bf2a506bd4f742596982425fa597abc1e10
Kevin Zelaya <smis091121@ugb.edu.sv>
2021-08-14 10:51:59 -0600

    Agregar archivos de proyecto.

---
+++ programacionclase1308/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programacionclase1308
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
